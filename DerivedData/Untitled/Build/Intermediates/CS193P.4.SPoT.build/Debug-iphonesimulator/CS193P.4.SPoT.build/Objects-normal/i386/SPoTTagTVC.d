8d996e94c13420707230fa43445385f61ad9c1e5
fxv <unmeldungen@gmail.com>
2013-02-24 11:54:49 +0100

    .gitignore updated

--- DerivedData/Untitled/Build/Intermediates/CS193P.4.SPoT.build/Debug-iphonesimulator/CS193P.4.SPoT.build/Objects-normal/i386/SPoTTagTVC.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/FV/Developer/CS193P/CS193P.4.SPoT/CS193P.4.SPoT/SPoTTagTVC.m \
-  /Users/FV/Developer/CS193P/CS193P.4.SPoT/CS193P.4.SPoT/SPoTTagTVC.h \
-  /Users/FV/Developer/CS193P/CS193P.4.SPoT/CS193P.4.SPoT/FlickrFetcher.h
